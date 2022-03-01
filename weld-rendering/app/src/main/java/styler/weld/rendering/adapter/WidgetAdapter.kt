20c0e6eadccf2fb0c16025156bf35c961366a8cc
Tan N. Truong <tan.truong@styler.link>
2022-03-01 20:15:59 +0900

    Change to BaseViewHolder

--- weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
@@ -6,16 +6,16 @@ import androidx.recyclerview.widget.RecyclerView
 import styler.weld.rendering.R
 import styler.weld.rendering.models.Widget
 import styler.weld.rendering.viewholder.BannerViewHolder
+import styler.weld.rendering.viewholder.BaseViewHolder
 
-class WidgetAdapter(private val response: Widget?) : RecyclerView.Adapter<BannerViewHolder>() {
+class WidgetAdapter(private val response: Widget?) : RecyclerView.Adapter<BaseViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
         // TODO: Banner type for now
         val view = LayoutInflater.from(parent.context).inflate(R.layout.banner_row_item, parent, false)
         return BannerViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
     }
 
     override fun getItemCount() = response?.widgets?.size ?: 1
