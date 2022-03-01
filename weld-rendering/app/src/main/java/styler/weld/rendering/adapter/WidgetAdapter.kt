2ab24b1a94cbfc376429877a109ed4c04a227c45
Tan N. Truong <tan.truong@styler.link>
2022-03-01 17:36:56 +0900

    Init widget adapter

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
@@ -0,0 +1,18 @@
+package styler.weld.rendering.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WidgetAdapter : RecyclerView.Adapter<WidgetViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WidgetViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WidgetViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
