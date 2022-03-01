6f3201ac313cfcb452835320af073f052d7dfedb
Tan N. Truong <tan.truong@styler.link>
2022-03-01 20:12:53 +0900

    Create BaseViewHolder

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/BaseViewHolder.kt
@@ -0,0 +1,7 @@
+package styler.weld.rendering.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+open class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
