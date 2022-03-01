d96e448e782d4fd39e0eba5247b6168ac6e054c3
Tan N. Truong <tan.truong@styler.link>
2022-03-01 17:52:34 +0900

    Init onCreateViewHolder

--- weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
@@ -1,11 +1,15 @@
 package styler.weld.rendering.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import styler.weld.rendering.R
 
 class WidgetAdapter : RecyclerView.Adapter<BannerViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
-        TODO("Not yet implemented")
+        // TODO: Banner type for now
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.banner_row_item, parent, false)
+        return BannerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
