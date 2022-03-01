9eb0f1c70e301bb3522e3d3705cfe82ddc5d07bb
Tan N. Truong <tan.truong@styler.link>
2022-03-01 17:39:16 +0900

    Init item list ViewHolder

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/adapter/ItemListViewHolder.kt
@@ -0,0 +1,7 @@
+package styler.weld.rendering.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
