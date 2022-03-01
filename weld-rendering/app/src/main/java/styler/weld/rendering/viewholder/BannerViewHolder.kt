aa754eea6c42f9151a43dc3b6649e13b238eec3b
Tan N. Truong <tan.truong@styler.link>
2022-03-01 20:13:01 +0900

    Make BaseViewHolder extendable

--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/BannerViewHolder.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/BannerViewHolder.kt
@@ -3,5 +3,5 @@ package styler.weld.rendering.viewholder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class BannerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class BannerViewHolder(itemView: View) : BaseViewHolder(itemView) {
 }
--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/ItemListViewHolder.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/ItemListViewHolder.kt
@@ -3,5 +3,5 @@ package styler.weld.rendering.viewholder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ItemListViewHolder(itemView: View) : BaseViewHolder(itemView) {
 }
