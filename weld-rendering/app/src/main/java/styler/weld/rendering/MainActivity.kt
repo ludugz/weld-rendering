21639b6fe2e5c6dca87ac605d237ab6187d7430b
Tan N. Truong <tan.truong@styler.link>
2022-03-01 16:38:18 +0900

    Init weld project

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/MainActivity.kt
@@ -0,0 +1,11 @@
+package styler.weld.rendering
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
