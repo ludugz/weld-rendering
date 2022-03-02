c2424a5cbfd9c630c332b4b9bd8197fdf5a47096
Tan N. Truong <tan.truong@styler.link>
2022-03-02 15:00:11 +0900

    Update BannerViewHolder with Banner model

--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/BannerViewHolder.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/BannerViewHolder.kt
@@ -1,7 +1,13 @@
 package styler.weld.rendering.viewholder
 
 import android.view.View
-import androidx.recyclerview.widget.RecyclerView
+import styler.weld.rendering.models.local.banner.Banner
 
-class BannerViewHolder(itemView: View) : BaseViewHolder(itemView) {
+class BannerViewHolder(
+    itemView: View,
+    bannerList: List<Banner>
+) : BaseViewHolder<Banner>(itemView, bannerList) {
+    override fun bindData() {
+        TODO("Not yet implemented")
+    }
 }
