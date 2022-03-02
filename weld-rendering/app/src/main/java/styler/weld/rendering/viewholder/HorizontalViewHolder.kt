38d412d2bdf29145fe44b83b3ab5fa730d0bb1a5
Tan N. Truong <tan.truong@styler.link>
2022-03-02 19:35:22 +0900

    Init VH for horizontal list

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/HorizontalViewHolder.kt
@@ -0,0 +1,14 @@
+package styler.weld.rendering.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import styler.weld.rendering.models.local.itemlist.ItemListItem
+
+class HorizontalViewHolder(
+    itemView: View,
+    val listData: List<ItemListItem>?
+) : RecyclerView.ViewHolder(itemView) {
+    fun bindData() {
+        // TODO: Implement later
+    }
+}
