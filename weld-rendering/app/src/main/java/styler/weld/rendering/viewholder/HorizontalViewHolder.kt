037e4337a308064a277e69e9b153673ff046c66f
Tan N. Truong <tan.truong@styler.link>
2022-03-03 14:53:26 +0900

    Setup view in HorizontalViewHolder

--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/HorizontalViewHolder.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/HorizontalViewHolder.kt
@@ -1,14 +1,23 @@
 package styler.weld.rendering.viewholder
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import styler.weld.rendering.R
 import styler.weld.rendering.models.local.itemlist.ItemListItem
+import styler.weld.rendering.utils.imageFromId
 
 class HorizontalViewHolder(
     itemView: View,
-    val listData: List<ItemListItem>?
+    private val listData: List<ItemListItem>?
 ) : RecyclerView.ViewHolder(itemView) {
+    private val textView: TextView = itemView.findViewById(R.id.text_view_brand_horizontal)
+    private val imageView: ImageView = itemView.findViewById(R.id.image_view_horizontal)
     fun bindData() {
-        // TODO: Implement later
+        val data = listData?.get(adapterPosition)
+        textView.text = data?.brand
+        Glide.with(itemView.context).load(data?.image?.let { imageFromId(it) }).into(imageView)
     }
 }
