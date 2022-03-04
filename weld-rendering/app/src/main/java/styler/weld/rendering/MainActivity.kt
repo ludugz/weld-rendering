eb1e1a56bf4dd7bc075337bca07c7f0f96ab3367
Tan N. Truong <tan.truong@styler.link>
2022-03-04 16:57:40 +0900

    Add JSON parse handling from GSON library

--- weld-rendering/app/src/main/java/styler/weld/rendering/MainActivity.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/MainActivity.kt
@@ -1,14 +1,22 @@
 package styler.weld.rendering
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 import styler.weld.rendering.adapter.WidgetAdapter
-import styler.weld.rendering.utils.fakeData
+import styler.weld.rendering.models.local.BaseData
+import styler.weld.rendering.models.remote.Widget
+import styler.weld.rendering.utils.GsonUtils
+import java.lang.reflect.Type
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var widgetData: Widget<BaseData>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        initData()
         setUpRecyclerView()
     }
 
@@ -25,4 +34,12 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         }
     }
+
+    private fun initData() {
+        val type: Type = object : TypeToken<Widget<BaseData>>() {}.type
+        widgetData = GsonUtils.instance!!.fromJson(responseFromFile(), type)
+    }
+
+    private fun responseFromFile() =
+        applicationContext.assets.open("response.json").bufferedReader().use { it.readText() }
 }
