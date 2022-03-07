a2985a53c1c169fcec143fff0d6ce160bb913add
Tan N. Truong <tan.truong@styler.link>
2022-03-07 19:01:16 +0900

    Make field in BaseItem non-nullable

--- weld-rendering/app/src/main/java/styler/weld/rendering/models/remote/BaseItem.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/models/remote/BaseItem.kt
@@ -1,15 +1,15 @@
 package styler.weld.rendering.models.remote
 
 open class BaseItem(
-    open val brand: String? = null,
+    open val brand: String = "",
     open val discount: Any? = null,
-    open val id: String? = null,
-    open val image: String? = null,
-    open val name: String? = null,
+    open val id: String = "",
+    open val image: String = "",
+    open val name: String = "",
     open val price_with_tax: Double? = null,
-    open val shop_id: String? = null,
-    open val type: String? = null,
-    open val logo: String? = null,
+    open val shop_id: String = "",
+    open val type: String = "",
+    open val logo: String = "",
     open val image_url: String? = null,
     open val url: String? = null,
     open val title: String? = null,
