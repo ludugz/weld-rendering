404ad3b5c4d14642e978ded09c144d0ef3dede10
Tan N. Truong <tan.truong@styler.link>
2022-03-07 11:55:29 +0900

    Update BaseData (add article list fields)

--- weld-rendering/app/src/main/java/styler/weld/rendering/models/local/BaseData.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/models/local/BaseData.kt
@@ -1,11 +1,22 @@
 package styler.weld.rendering.models.local
 
 import styler.weld.rendering.models.local.itemlist.ItemListItem
+import styler.weld.rendering.models.local.shoplist.ShopListItem
 
 open class BaseData(
     // banner
     open val image_id: String? = null,
     open val target: String? = null,
+
     // item list
-    open val items: List<ItemListItem>? = null
+    open val items: List<ItemListItem>? = null,
+
+    // shop list
+    open val shops: List<ShopListItem>? = null,
+
+    // article List
+    open val image_url: String? = null,
+    open val url: String? = null,
+    open val title: String? = null,
+    open val excerpt: String? = null,
 ) : Any()
