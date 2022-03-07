8992817a56ec8a7f7c10138ad25774d7916a80ea
Tan N. Truong <tan.truong@styler.link>
2022-03-07 18:06:00 +0900

    Update main adapter

--- weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/adapter/WidgetAdapter.kt
@@ -1,21 +1,19 @@
 package styler.weld.rendering.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import styler.weld.rendering.R
 import styler.weld.rendering.models.local.BaseData
 import styler.weld.rendering.models.remote.Widget
-import styler.weld.rendering.models.remote.WidgetData
 import styler.weld.rendering.viewholder.*
 
 class WidgetAdapter(private val response: Widget<BaseData>?) :
-    RecyclerView.Adapter<BaseViewHolder<WidgetData<BaseData>>>() {
+    RecyclerView.Adapter<BaseViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): BaseViewHolder<WidgetData<BaseData>> {
+    ): BaseViewHolder {
         return when (viewType) {
             BANNER_TYPE -> {
                 bannerViewHolder(parent)
@@ -28,6 +26,9 @@ class WidgetAdapter(private val response: Widget<BaseData>?) :
             }
             SHOP_LIST -> {
                 shopListViewHolder(parent)
+            }
+            ARTICLE_LIST -> {
+                articleListViewHolder(parent)
             }
             else -> emptyViewHolder(parent)
         }
@@ -47,6 +48,13 @@ class WidgetAdapter(private val response: Widget<BaseData>?) :
         return ShopListViewHolder(view, response?.widgets)
     }
 
+    private fun articleListViewHolder(parent: ViewGroup): ArticleListViewHolder {
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.article_list_row_item, parent, false)
+        return ArticleListViewHolder(view, response?.widgets)
+    }
+
     private fun bannerViewHolder(parent: ViewGroup): BannerViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.banner_row_item, parent, false)
@@ -56,10 +64,10 @@ class WidgetAdapter(private val response: Widget<BaseData>?) :
     private fun emptyViewHolder(parent: ViewGroup): EmptyViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.empty_view_row_item, parent, false)
-        return EmptyViewHolder(view, response?.widgets)
+        return EmptyViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: BaseViewHolder<WidgetData<BaseData>>, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         holder.bindData()
     }
 
@@ -73,6 +81,7 @@ class WidgetAdapter(private val response: Widget<BaseData>?) :
             "item_list" -> ITEM_LIST_TYPE
             "local_recent_items" -> LOCAL_RECENT_ITEMS
             "shop_list" -> SHOP_LIST
+            "article_list" -> ARTICLE_LIST
             else -> INVALID_TYPE
         }
     }
@@ -83,5 +92,6 @@ class WidgetAdapter(private val response: Widget<BaseData>?) :
         private const val ITEM_LIST_TYPE = 2
         private const val LOCAL_RECENT_ITEMS = 3
         private const val SHOP_LIST = 4
+        private const val ARTICLE_LIST = 5
     }
 }
