4d5061825119f57ee32a8f1546c8d3d6969c61d0
Tan N. Truong <tan.truong@styler.link>
2022-03-07 18:07:55 +0900

    Init Article List View Holder

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/ArticleListViewHolder.kt
@@ -0,0 +1,32 @@
+package styler.weld.rendering.viewholder
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import styler.weld.rendering.R
+import styler.weld.rendering.adapter.ArticleAdapter
+import styler.weld.rendering.models.local.BaseData
+import styler.weld.rendering.models.remote.WidgetData
+
+class ArticleListViewHolder(
+    itemView: View,
+    private val widgetData: List<WidgetData<BaseData>>?
+) : BaseViewHolder(itemView) {
+    private val tvLabel: TextView = itemView.findViewById(R.id.text_view_label_article)
+    private val recyclerView: RecyclerView = itemView.findViewById(R.id.recycler_view_article)
+
+    override fun bindData() {
+        widgetData?.forEach { widgetData ->
+            if (widgetData.type == "article_list") {
+                val data = widgetData.data.items
+                recyclerView.apply {
+                    adapter = ArticleAdapter(data, "article")
+                    layoutManager =
+                        LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
+                }
+                tvLabel.text = widgetData.title
+            }
+        }
+    }
+}
