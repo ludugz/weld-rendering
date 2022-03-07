34d54ba9d7dff41035175f7f3e6f161e2cb47cc6
Tan N. Truong <tan.truong@styler.link>
2022-03-07 11:58:04 +0900

    Remove unused models

--- weld-rendering/app/src/main/java/styler/weld/rendering/models/local/banner/BannerData.kt
+++
@@ -1,8 +0,0 @@
-package styler.weld.rendering.models.local.banner
-
-import styler.weld.rendering.models.local.BaseData
-
-data class BannerData(
-    override val image_id: String,
-    override val target: String
-) : BaseData()
--- weld-rendering/app/src/main/java/styler/weld/rendering/models/local/itemlist/ItemListData.kt
+++
@@ -1,7 +0,0 @@
-package styler.weld.rendering.models.local.itemlist
-
-import styler.weld.rendering.models.local.BaseData
-
-data class ItemListData(
-    override val items: List<ItemListItem>
-) : BaseData()
