75f1357ae3039d133569471d65a67a3606c0ee07
Tan N. Truong <tan.truong@styler.link>
2022-03-07 18:04:59 +0900

    Add model for Article

---
+++ weld-rendering/app/src/main/java/styler/weld/rendering/models/local/articlelist/ArticleListItem.kt
@@ -0,0 +1,10 @@
+package styler.weld.rendering.models.local.articlelist
+
+import styler.weld.rendering.models.local.BaseItem
+
+data class ArticleListItem(
+    override val image_url: String? = null,
+    override val url: String? = null,
+    override val title: String? = null,
+    override val excerpt: String? = null
+) : BaseItem()
