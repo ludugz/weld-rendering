49ac690752022b7f33508c91e5d7d8d9d5018acd
Tan N. Truong <tan.truong@styler.link>
2022-03-11 16:08:06 +0900

    Replace when if/else by Lambda in FactoryMethod

--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/WidgetFactory.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/WidgetFactory.kt
@@ -8,8 +8,8 @@ class WidgetFactory {
 
     var factories: MutableList<FactoryMethod> = mutableListOf();
 
-    fun register(type: String) {
-        factories.add(FactoryMethod(type))
+    fun register(type: String, factory: (parent: ViewGroup) -> BaseViewHolder) {
+        factories.add(FactoryMethod(type, factory))
     }
 
     fun createViewHolder(viewType: Int, parent: ViewGroup): BaseViewHolder {
@@ -36,25 +36,7 @@ class WidgetFactory {
     }
 }
 
-class FactoryMethod(val type: String) {
-    fun createViewHolder(parent: ViewGroup): BaseViewHolder {
-        return when (type) {
-            "banner" -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.banner_row_item, parent, false)
-                BannerViewHolder(view)
-            }
-            "item_list" -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_list_row_item, parent, false)
-                ItemListViewHolder(view)
-            }
-            else -> {
-                val view = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.empty_view_row_item, parent, false)
-                EmptyViewHolder(view)
-            }
-        }
-    }
+class FactoryMethod(val type: String, val factory: (parent: ViewGroup) -> BaseViewHolder) {
+    fun createViewHolder(parent: ViewGroup): BaseViewHolder = factory(parent)
 }
 
