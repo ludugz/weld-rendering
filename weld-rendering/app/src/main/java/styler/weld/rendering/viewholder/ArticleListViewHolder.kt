c1ba4f5c2c97c87e581a2f26549f320d72598122
Tan N. Truong <tan.truong@styler.link>
2022-03-11 16:09:13 +0900

    Update article list view holder

--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/ArticleListViewHolder.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/ArticleListViewHolder.kt
@@ -1,13 +1,12 @@
 package styler.weld.rendering.viewholder
 
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import styler.weld.rendering.R
 import styler.weld.rendering.adapter.ArticleAdapter
+import styler.weld.rendering.models.local.articlelist.ArticleListItem
 import styler.weld.rendering.models.remote.WidgetData
 
 class ArticleListViewHolder(
@@ -16,19 +15,24 @@ class ArticleListViewHolder(
     private val tvLabel: TextView = itemView.findViewById(R.id.text_view_label_article)
     private val recyclerView: RecyclerView = itemView.findViewById(R.id.recycler_view_article)
 
-    companion object {
-        fun instance(parent: ViewGroup): BaseViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.banner_row_item, parent, false)
-            return ArticleListViewHolder(view)
+    override fun bindData(data: WidgetData) {
+        recyclerView.apply {
+            adapter = ArticleAdapter(articleListFromBaseData(data), "article")
+            layoutManager =
+                LinearLayoutManager(itemView.context, LinearLayoutManager.VERTICAL, false)
         }
+        tvLabel.text = data.title
     }
 
-    override fun bindData(data: WidgetData) {
-//        recyclerView.apply {
-//            adapter = ArticleAdapter(data.data, "article")
-//            layoutManager =
-//                LinearLayoutManager(itemView.context, LinearLayoutManager.VERTICAL, false)
-//        }
-//        tvLabel.text = data.title
+    private fun articleListFromBaseData(data: WidgetData): List<ArticleListItem>? {
+        return (data.data["items"] as List<*>?)?.map {
+            it as Map<*, *>
+            ArticleListItem(
+                it["image_url"] as String,
+                it["url"] as String,
+                it["title"] as String,
+                it["excerpt"] as String,
+            )
+        }
     }
 }
