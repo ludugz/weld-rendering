1d3aa3b91f38eee544e555a06458b808a0d8ca20
Tan N. Truong <tan.truong@styler.link>
2022-03-11 16:47:45 +0900

    Delete unnecessary models

--- weld-rendering/app/src/main/java/styler/weld/rendering/models/remote/Data.kt
+++
@@ -1,9 +0,0 @@
-package styler.weld.rendering.models.remote
-
-import styler.weld.rendering.models.remote.BaseItem
-
-open class Data(
-    open val image_id: String? = null,
-    open val target: String? = null,
-    open val items: List<BaseItem>? = null,
-)
