e3f7636caa3fd24e4ea7c60b5149e57ff0e3082e
Tan N. Truong <tan.truong@styler.link>
2022-03-11 16:08:41 +0900

    Handle invalid type of view

--- weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/WidgetFactory.kt
+++ weld-rendering/app/src/main/java/styler/weld/rendering/viewholder/WidgetFactory.kt
@@ -1,8 +1,6 @@
 package styler.weld.rendering.viewholder
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import styler.weld.rendering.R
 
 class WidgetFactory {
 
@@ -13,26 +11,16 @@ class WidgetFactory {
     }
 
     fun createViewHolder(viewType: Int, parent: ViewGroup): BaseViewHolder {
-        if (viewType == INVALID) {
-            // TODO: INVALID case should be refactored later
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.empty_view_row_item, parent, false)
-            return EmptyViewHolder(view)
-        }
         return factories[viewType].createViewHolder(parent)
     }
 
     fun getViewTypeFor(type: String): Int {
         val factory = factories.findLast { it.type == type }
         return if (factory == null) {
-            INVALID
+            factories.lastIndex
         } else {
             factories.indexOf(factory)
         }
-    }
-
-    companion object {
-        const val INVALID = -1
     }
 }
 
